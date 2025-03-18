634fd6df22ea04563bb9c26b55668785b7191b9c
francocastano <francomatiascastano@gmail.com>
2025-03-18 15:32:42 -0300

    exercise2

---
+++ exercise2.py
@@ -0,0 +1,20 @@
+"""""
+Ejercicio 2
+
+Poner paréntesis en las siguientes expresiones de acuerdo a las reglas de precedencia y asociatividad de los operadores.
+Si no conoce la precedencia de algún operador, se aconseja probarla primero en el intérprete.
+Una vez hecho esto, determinar el tipo de cada subexpresión entre paréntesis hasta determinar el tipo de la expresión completa.
+
+
+"""""
+
+a = 3 * 5 - 7 * 4 / 14 + 3 / 1
+b = 2 ** 1 + 3 / 5 // 4
+c = 8 / 4 / 2 ** 2 ** 2
+
+
+d = (3 * 5) - ((7 * 4) / 14) + (3 / 1)
+f = (2 ** 1) + ((3 / 5) // 4)
+# e = (((8 / 4) / 2) ** (2 ** 2))
+
+print(c,e)
